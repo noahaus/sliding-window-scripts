7aa0bdf89c264a92a5053be815287b01dc863806
Noah Legall <noahaus@uga.edu>
2021-01-01 16:37:43 -0500

    basic fasta parsing\

--- tjd/src/main.rs
+++ tjd/src/main.rs
@@ -5,19 +5,32 @@ purpose: given a multi-sequence alignment, calculate the Tajima's D statistic in
 */
 fn main() {
 use seq_io::fasta::{Reader,Record}; //https://docs.rs/seq_io/0.3.1/seq_io/
+use std::str;
 
+// read in the initial fasta file
 let mut reader = Reader::from_path("example.fa").unwrap();
 
-let mut n = 0;
-let mut sum = 0;
+
+// is there a way to create specific windows of a fasta?
+// well, can we get the fasta in a form we want first?
+// A vec of Strings is my go to as a novice Rust programmer.
+let mut window = Vec::<String>::new();
 while let Some(record) = reader.next() {
     let record = record.expect("Error reading record");
     for s in record.seq_lines() {
-        sum += s.len();
+        // this is where the magic should happen
+        // window takes the value of the FASTA record 
+        // it comes out as a array of utf numbers. this can be converted into characters.
+        window.push(str::from_utf8(&s).unwrap().to_string());
     }
-    n += 1;
+
 }
-println!("mean sequence length of {} records: {:.1} bp", n, sum as f32 / n as f32);   
+// we now successfully have a list of sequences. 
+// uncomment the print statement to check.
+// println!("{:?}",window);
+
+//TODO: I'll need to figure out how to take the vec of strings and perform a sliding window analysis on it.
+
 
 
 }
