2c2cb4994c3972c090aadf5c987037d047c67040
Noah Legall <noahaus@ss-sub2.gacrc.uga.edu>
2021-01-22 09:31:13 -0500

    mandatory commit

--- tjd/target/debug/tjd.d
+++ tjd/target/debug/tjd.d
@@ -1,1 +1,1 @@
-/Users/noahaus/GithubRepos/sliding-window-scripts/tjd/target/debug/tjd: /Users/noahaus/GithubRepos/sliding-window-scripts/tjd/src/main.rs
+/scratch/noahaus/bov_pangen_reservoirs/core/positive_selection/species_tjd/sliding-window-scripts/tjd/target/debug/tjd: /scratch/noahaus/bov_pangen_reservoirs/core/positive_selection/species_tjd/sliding-window-scripts/tjd/src/main.rs
